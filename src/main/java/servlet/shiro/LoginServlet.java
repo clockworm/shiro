88862325012062e00bb6b4e88f978a43cca1ac3b
mick <mick@192.168.1.105>
2017-05-25 23:52:51 +0800

    test fail

---
+++ src/main/java/servlet/shiro/LoginServlet.java
@@ -0,0 +1,25 @@
+package servlet.shiro;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.err.println("登录页面");
+		req.getRequestDispatcher("login.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doPost(req, resp);
+	}
+
+	
+}
